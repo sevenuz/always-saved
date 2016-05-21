6edf58065aadbcab6dd8d5d8bbc0ff6844467431
sevenuz <julius@athenstaedt.net>
2016-05-21 18:15:04 +0200

    Atom Package which automaticaly saves the current file
    During editing a file and when this package is active your file is saved
    instantly

---
+++ spec/always-saved-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AlwaysSavedView from '../lib/always-saved-view';
+
+describe('AlwaysSavedView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
